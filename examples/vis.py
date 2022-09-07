0030669651155e4199561e50774f931c5816dc7b
Yucheng Yin <yyin4@andrew.cmu.edu>
2022-09-06 20:52:26 -0400

    visual

---
+++ examples/vis.py
@@ -0,0 +1,11 @@
+from netshare import Generator
+
+if __name__ == '__main__':
+    # configuration file
+    generator = Generator(config="netflow/config_example_netflow_nodp.json")
+
+    # `work_folder` should not exist o/w an overwrite error will be thrown.
+    # Please set the `worker_folder` as *absolute path*
+    # if you are using Ray with multi-machine setup
+    # since Ray has bugs when dealing with relative paths.
+    generator.visualize(work_folder='../results/test')
