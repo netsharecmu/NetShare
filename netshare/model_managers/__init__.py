fcdae74c4681d963eacfcb8ca46c6c9c42891569
Zinan Lin <linzinan1995@126.com>
2022-10-09 22:50:50 -0700

    Add DG model manager

--- netshare/model_managers/__init__.py
+++ netshare/model_managers/__init__.py
@@ -1,4 +1,5 @@
 from .model_manager import ModelManager
 from .netshare_manager.netshare_manager import NetShareManager
+from .dg_model_manager import DGModelManager
 
-__all__ = ['ModelManager', 'NetShareManager']
+__all__ = ['ModelManager', 'NetShareManager', 'DGModelManager']
