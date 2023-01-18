648eae38fb887b570ac457e27be3b64f555460a2
Minhao Jin <minhaoj@andrew.cmu.edu>
2023-01-18 18:09:26 -0500

    bug fix on merge_attr

--- netshare/generate/model_generate.py
+++ netshare/generate/model_generate.py
@@ -11,6 +11,7 @@ from tqdm import tqdm
 import netshare.utils.ray as ray
 from netshare import models
 from netshare.configs import get_config
+from netshare.learn import learn_api
 from netshare.utils.logger import TqdmToLogger, logger
 from netshare.utils.model_configuration import create_chunks_configurations
 from netshare.utils.paths import get_generated_data_log_folder
@@ -72,19 +73,25 @@ def generate_data(
 @ray.remote(scheduling_strategy="SPREAD", max_calls=1)
 def merge_attr(config_group: dict, configs: List[dict]) -> None:
     """
-    TODO: Can someone help me to doc this function?
+    This is the function to merge the attributes. The models trained on different
+    chunks of data will first generate attributes. Then, this function will be
+    invoked to merge the synthetic attributes. Given the merged attributes, models
+    will continue generating the time-series measurements.
+
+    :return: None
     """
     chunk0_idx = config_group["config_ids"][0]
     eval_root_folder = configs[chunk0_idx]["eval_root_folder"]
     attr_raw_npz_folder = os.path.join(eval_root_folder, "attr_raw")
-    word2vec_size = configs[chunk0_idx].get("word2vec_vecSize", 0)
-    pcap_interarrival = configs[chunk0_idx].get("timestamp") == "interarrival"
     num_chunks = len(config_group["config_ids"])
 
-    if not pcap_interarrival:
-        bit_idx_flagstart = 128 + word2vec_size * 3
-    else:
-        bit_idx_flagstart = 128 + word2vec_size * 3 + 1
+    dim = 0
+    for field in list(learn_api.get_attributes_fields().values()):
+        if field.name != "startFromThisChunk":
+            dim += field.getOutputDim()
+        else:
+            break
+    col_idx_of_session_start = dim
 
     attr_clean_npz_folder = os.path.join(
         str(Path(attr_raw_npz_folder).parents[0]), "attr_clean"
@@ -112,44 +119,28 @@ def merge_attr(config_group: dict, configs: List[dict]) -> None:
             os.path.join(attr_raw_npz_folder, "chunk_id-{}.npz".format(chunkid))
         )["data_attribute"]
 
-        if num_chunks > 1 and word2vec_size:
+        if num_chunks > 1:
             for row in raw_attr_chunk:
-                # if row[bit_idx_flagstart] < row[bit_idx_flagstart+1]:
                 if (
-                    row[bit_idx_flagstart] < row[bit_idx_flagstart + 1]
-                    and row[bit_idx_flagstart + 2 * chunkid + 2]
-                    < row[bit_idx_flagstart + 2 * chunkid + 3]
+                    row[col_idx_of_session_start] < row[col_idx_of_session_start + 1]
+                    and row[col_idx_of_session_start + 2 * chunkid + 2]
+                    < row[col_idx_of_session_start + 2 * chunkid + 3]
                 ):
                     # this chunk
-                    row_this_chunk = list(copy.deepcopy(row)[:bit_idx_flagstart])
-                    row_this_chunk += [0.0, 1.0]
-                    row_this_chunk += [1.0, 0.0] * (chunkid + 1)
-                    for i in range(chunkid + 1, num_chunks):
-                        if (
-                            row[bit_idx_flagstart + 2 * i + 2]
-                            < row[bit_idx_flagstart + 2 * i + 3]
-                        ):
-                            row_this_chunk += [0.0, 1.0]
-                        else:
-                            row_this_chunk += [1.0, 0.0]
-                    # dict_chunkid_attr[chunkid].append(row_this_chunk)
                     dict_chunkid_attr[chunkid].append(row)
 
                     # following chunks
-                    # row_following_chunk = list(copy.deepcopy(row)[:bit_idx_flagstart])
-                    # row_following_chunk += [1.0, 0.0]*(1+NUM_CHUNKS)
                     n_flows_startFromThisEpoch += 1
                     row_following_chunk = list(copy.deepcopy(row))
-                    row_following_chunk[bit_idx_flagstart] = 1.0
-                    row_following_chunk[bit_idx_flagstart + 1] = 0.0
+                    row_following_chunk[col_idx_of_session_start] = 1.0
+                    row_following_chunk[col_idx_of_session_start + 1] = 0.0
 
                     for i in range(chunkid + 1, num_chunks):
                         if (
-                            row[bit_idx_flagstart + 2 * i + 2]
-                            < row[bit_idx_flagstart + 2 * i + 3]
+                            row[col_idx_of_session_start + 2 * i + 2]
+                            < row[col_idx_of_session_start + 2 * i + 3]
                         ):
                             dict_chunkid_attr[i].append(row_following_chunk)
-                            # dict_chunkid_attr[i].append(row)
         else:
             dict_chunkid_attr[chunkid] = raw_attr_chunk
 
@@ -160,15 +151,3 @@ def merge_attr(config_group: dict, configs: List[dict]) -> None:
             os.path.join(attr_clean_npz_folder, "chunk_id-{}.npz".format(chunkid)),
             data_attribute=np.asarray(attr_clean),
         )
-
-
-# ===================== TODO: move merge_syn_df to postprocess ================
-
-
-def get_per_chunk_df(chunk_folder: str) -> pd.DataFrame:
-    '''chunk_folder: "chunk_id-0"'''
-    df_names = [file for file in os.listdir(chunk_folder) if file.endswith(".csv")]
-    assert len(df_names) > 0
-    df = pd.read_csv(os.path.join(chunk_folder, df_names[0]))
-
-    return df
