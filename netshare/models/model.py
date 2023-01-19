be3d0378705c1624d09bf603b0ec8d0c718020c9
Minhao Jin <minhaoj@andrew.cmu.edu>
2023-01-18 23:09:41 -0500

    make pre-commit happy

--- netshare/models/model.py
+++ netshare/models/model.py
@@ -1,5 +1,5 @@
-from abc import ABC, abstractmethod
 import os
+from abc import ABC, abstractmethod
 
 from netshare.utils import Tee
 
@@ -13,26 +13,38 @@ class Model(ABC):
         ...
 
     @abstractmethod
-    def _generate(self, input_train_data_folder,
-                  input_model_folder, output_syn_data_folder, log_folder):
+    def _generate(
+        self,
+        input_train_data_folder,
+        input_model_folder,
+        output_syn_data_folder,
+        log_folder,
+    ):
         ...
 
     def train(self, input_train_data_folder, output_model_folder, log_folder):
-        stdout_log_path = os.path.join(log_folder, 'model.train.stdout.log')
-        stderr_log_path = os.path.join(log_folder, 'model.train.stderr.log')
+        stdout_log_path = os.path.join(log_folder, "model.train.stdout.log")
+        stderr_log_path = os.path.join(log_folder, "model.train.stderr.log")
         with Tee(stdout_path=stdout_log_path, stderr_path=stderr_log_path):
             return self._train(
                 input_train_data_folder=input_train_data_folder,
                 output_model_folder=output_model_folder,
-                log_folder=log_folder)
+                log_folder=log_folder,
+            )
 
-    def generate(self, input_train_data_folder, input_model_folder,
-                 output_syn_data_folder, log_folder):
-        stdout_log_path = os.path.join(log_folder, 'model.generate.stdout.log')
-        stderr_log_path = os.path.join(log_folder, 'model.generate.stderr.log')
+    def generate(
+        self,
+        input_train_data_folder,
+        input_model_folder,
+        output_syn_data_folder,
+        log_folder,
+    ):
+        stdout_log_path = os.path.join(log_folder, "model.generate.stdout.log")
+        stderr_log_path = os.path.join(log_folder, "model.generate.stderr.log")
         with Tee(stdout_path=stdout_log_path, stderr_path=stderr_log_path):
             return self._generate(
                 input_train_data_folder=input_train_data_folder,
                 input_model_folder=input_model_folder,
                 output_syn_data_folder=output_syn_data_folder,
-                log_folder=log_folder)
+                log_folder=log_folder,
+            )
