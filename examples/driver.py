fde7a0afa00bd3f57d64d27a1816e9135a943c86
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-02-08 10:06:09 -0500

    update pcap driver

--- examples/driver.py
+++ examples/driver.py
@@ -1,17 +1,15 @@
-import netshare.ray as ray
+import random
+import time
+
 from netshare import Generator
 
+random.seed(time.time())
+
 if __name__ == "__main__":
-    ray.config.enabled = True
-    ray.init(address="auto")
-
-    # configuration file
-    generator = Generator(config="pcap/config_example_pcap_nodp.json")
-
-    # `work_folder` should not exist o/w an overwrite error will be thrown.
-    # Please set the `worker_folder` as *absolute path*
-    # if you are using Ray with multi-machine setup
-    # since Ray has bugs when dealing with relative paths.
-    generator.train_and_generate(work_folder="../results/test")
-
-    ray.shutdown()
+    generator = Generator(
+        # configuration file
+        config="pcap/config_example_pcap_nodp.json",
+        # `work_folder` should not exist o/w an overwrite error will be thrown.
+        work_folder="../results/test_caida-" + str(random.randint(0, 1000000)),
+    )
+    generator.train_and_generate()
