d0170630855b0f7626649ebb4bf72dd5d69a71fe
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-02-15 10:08:34 -0500

    bring back netflow example

--- examples/pcap/driver.py
+++ examples/pcap/driver.py
@@ -8,10 +8,10 @@ random.seed(time.time())
 if __name__ == "__main__":
     generator = Generator(
         # configuration file
-        config="pcap/config_example_pcap_nodp.json",
+        config="config_example_pcap_nodp.json",
         # `work_folder` should not exist o/w an overwrite error will be thrown.
-        work_folder="../results/test_caida-" + str(random.randint(0, 1000000)),
-        # work_folder="../results/test_caida-561722",
+        work_folder="../../results/test_caida-" + str(random.randint(0, 1000000)),
+        # work_folder="../../results/test_caida-561722",
     )
     generator.train_and_generate()
     # generator.generate()
