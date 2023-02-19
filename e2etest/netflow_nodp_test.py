f659e35d602aa6f05cba2cd1be34768f952282ad
koseoyoung <dorothy0602@gmail.com>
2023-02-19 13:33:26 -0500

    test: add a simple netflow e2e test

--- e2etest/netflow_nodp_test.py
+++ e2etest/netflow_nodp_test.py
@@ -1,15 +1,20 @@
+import pytest
 import random
 import time
+import os.path
 
 from netshare import Generator
 
-random.seed(time.time())
+random.seed(time.time())    
 
-if __name__ == "__main__":
+def test_driver_netflow_nodp():
+    randValue = str(random.randint(0, 1000000))
     generator = Generator(
         # configuration file
         config="../examples/netflow/config_example_netflow_nodp.json",
         # `work_folder` should not exist o/w an overwrite error will be thrown.
-        work_folder="../results/test_ugr16-" + str(random.randint(0, 1000000)),
+        work_folder="../results/test_ugr16-" + randValue,
     )
     generator.train_and_generate()
+    
+    assert os.path.isdir("../results/test_ugr16-" + randValue) == True
