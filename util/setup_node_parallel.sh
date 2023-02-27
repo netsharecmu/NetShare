4ba809d13bd85f6919238d99f84bf8b8fce2efca
Yucheng Yin <yyin4@andrew.cmu.edu>
2023-02-27 15:27:59 -0500

    update config details on large-scale tests

--- util/setup_node_parallel.sh
+++ util/setup_node_parallel.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
 USER=yyucheng
-NUMHOSTS=21
-EXPERIMENTNAME=netshare-d430
+NUMHOSTS=11
+EXPERIMENTNAME=ns-test
 PROJECTNAME=cloudmigration-pg0
-# LOCATION=utah
-LOCATION=wisc
+LOCATION=utah
+# LOCATION=wisc
 # LOCATION=clemson
 SITE=cloudlab.us
 
 pids=()
 
 # setup controller
-# NODE_SYSTEM="${USER}@nfs.${EXPERIMENTNAME}.${PROJECTNAME}.${LOCATION}.${SITE}"
-NODE_SYSTEM="${USER}@nfs.${EXPERIMENTNAME}.cloudmigration.emulab.net"
+NODE_SYSTEM="${USER}@nfs.${EXPERIMENTNAME}.${PROJECTNAME}.${LOCATION}.${SITE}"
+# NODE_SYSTEM="${USER}@nfs.${EXPERIMENTNAME}.cloudmigration.emulab.net"
 echo $NODE_SYSTEM
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $NODE_SYSTEM "sudo -n env RESIZEROOT=192 bash -s" < grow-rootfs.sh
-# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $NODE_SYSTEM "bash -s" < setup-cpu.sh "NetShare" & 
-# scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/netshare-package $NODE_SYSTEM:~/.ssh/id_rsa &
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $NODE_SYSTEM "bash -s" < setup-cpu.sh "NetShare" & 
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/netshare-package $NODE_SYSTEM:~/.ssh/id_rsa &
 pids+=($!)
 
 # setup workers
-# COUNTER=1
-# while [  $COUNTER -lt $NUMHOSTS ]; do
-#     NODE="node${COUNTER}" 
-#     # NODE_SYSTEM="${USER}@${NODE}.${EXPERIMENTNAME}.${PROJECTNAME}.${LOCATION}.${SITE}"
-#     NODE_SYSTEM="${USER}@${NODE}.${EXPERIMENTNAME}.cloudmigration.emulab.net"
-#     echo $NODE_SYSTEM
+COUNTER=1
+while [  $COUNTER -lt $NUMHOSTS ]; do
+    NODE="node${COUNTER}" 
+    NODE_SYSTEM="${USER}@${NODE}.${EXPERIMENTNAME}.${PROJECTNAME}.${LOCATION}.${SITE}"
+    # NODE_SYSTEM="${USER}@${NODE}.${EXPERIMENTNAME}.cloudmigration.emulab.net"
+    echo $NODE_SYSTEM
 
-#     ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $NODE_SYSTEM "sudo -n env RESIZEROOT=192 bash -s" < grow-rootfs.sh
-#     ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $NODE_SYSTEM "bash -s" < setup-cpu.sh "NetShare" & 
-#     scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/netshare-package $NODE_SYSTEM:~/.ssh/id_rsa &
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $NODE_SYSTEM "sudo -n env RESIZEROOT=192 bash -s" < grow-rootfs.sh
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $NODE_SYSTEM "bash -s" < setup-cpu.sh "NetShare" & 
+    scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.ssh/netshare-package $NODE_SYSTEM:~/.ssh/id_rsa &
 
-#     pids+=($!)
-#     let COUNTER=COUNTER+1
-# done
+    pids+=($!)
+    let COUNTER=COUNTER+1
+done
 
 for pid in "${pids[@]}"; do
     wait "$pid"
