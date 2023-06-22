516fbd432ed369c5bc9885443968ba92554be2bd
Minhao Jin <minhaoj@andrew.cmu.edu>
2023-06-22 11:04:59 -0400

    update example

---
+++ examples/netflow-ugr-100k-oci/driver.py
@@ -0,0 +1,21 @@
+import random
+import netshare.ray as ray
+from netshare import Generator
+
+if __name__ == '__main__':
+    # Change to False if you would not like to use Ray
+    ray.config.enabled = False
+    ray.init(address="auto")
+
+    # configuration file
+    generator = Generator(config="config_example_netflow_nodp.json")
+
+    # `work_folder` should not exist o/w an overwrite error will be thrown.
+    # Please set the `worker_folder` as *absolute path*
+    # if you are using Ray with multi-machine setup
+    # since Ray has bugs when dealing with relative paths.
+    generator.train(work_folder=f'~/results/ugr-100k')
+    generator.generate(work_folder=f'~/results/ugr-100k')
+    generator.visualize(work_folder=f'~/results/ugr-100k')
+
+    ray.shutdown()
---
+++ examples/pcap-m57-500k/driver.py
@@ -0,0 +1,24 @@
+import random
+import netshare.ray as ray
+from netshare import Generator
+import torch
+
+if __name__ == '__main__':
+    # Change to False if you would not like to use Ray
+    ray.config.enabled = False
+    if ray.config.enabled:
+        raise Exception("Ray cannot be launched when running on a single node with multiple gpus")
+    ray.init(address="auto")
+
+    # configuration file
+    generator = Generator(config="config_example_pcap_nodp.json")
+
+    # `work_folder` should not exist o/w an overwrite error will be thrown.
+    # Please set the `worker_folder` as *absolute path*
+    # if you are using Ray with multi-machine setup
+    # since Ray has bugs when dealing with relative paths.
+    generator.train(work_folder=f'/lustre/minhao/results/m57-800k')
+    generator.generate(work_folder=f'/lustre/minhao/results/m57-800k')
+    generator.visualize(work_folder=f'/lustre/minhao/results/m57-800k')
+
+    ray.shutdown()
